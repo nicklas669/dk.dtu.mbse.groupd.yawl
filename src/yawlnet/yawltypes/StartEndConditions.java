6ed398fdb1c13eba15808da1f1277492c37f67b5
nicklas669 <nicklas669@gmail.com>
2016-05-10 09:58:33 +0200

    Start must not have any inputs End must not have any outputs

--- src/yawlnet/yawltypes/StartEndConditions.java
+++ src/yawlnet/yawltypes/StartEndConditions.java
@@ -3,16 +3,19 @@ package yawlnet.yawltypes;
 import java.util.Iterator;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
+import org.pnml.tools.epnk.pnmlcoremodel.Arc;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
 
 /**
  * This is a constraint saying that a YAWL net should have exactly one start place and one
  * end place.
  *
- * @author ekki@dtu.dk
+ * @author Nicklas Hansen & Emil Hvid
+ * Inspiration from ekki@dtu.dk
  * @generated NOT
  */
 public class StartEndConditions extends AbstractModelConstraint {
@@ -24,6 +27,7 @@ public class StartEndConditions extends AbstractModelConstraint {
 			if (container instanceof PetriNet) {
 				int startCount = 0;
 				int endCount = 0;
+				boolean startHasArcs = false, endHasArcs = false;
 				Iterator<EObject> iterator = container.eAllContents();
 				while (iterator.hasNext()) {
 					EObject content = iterator.next();
@@ -42,9 +46,13 @@ public class StartEndConditions extends AbstractModelConstraint {
 							// If Place's type is start or end, increment counter for it
 							case PlaceTypes.END_VALUE:
 								endCount++;
+								// If end place has any outgoing arcs
+								if (place.getOut().size() > 0) endHasArcs = true;
 								break;
 							case PlaceTypes.START_VALUE:
 								startCount++;
+								// If start place has any incoming arcs
+								if (place.getIn().size() > 0) startHasArcs = true;
 								break;
 							}
 						}
@@ -53,9 +61,14 @@ public class StartEndConditions extends AbstractModelConstraint {
 				if (startCount != 1 || endCount != 1) {
 					// if there is not exactly one start place and exactly one
 					// end place, return a failure status.
-					return ctx.createFailureStatus(new Object[] {container});
+					return ctx.createFailureStatus("The YAWL net "+(new Object[] {container})+" does not have exactly one start place and one end place.");
+					//return ctx.createFailureStatus(new Object[] {container});
+					//return ctx.create
 					
 				}
+				
+				if (endHasArcs)	return ctx.createFailureStatus("An end place has output Arcs!");
+				if (startHasArcs) return ctx.createFailureStatus("A start place has input Arcs!");
 				
 			}
 
